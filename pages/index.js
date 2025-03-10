3e70050c8d6e79f1976ec89c108aa419b433da0d
yoyoartworld <yoyoartworld@gmail.com>
2025-03-10 13:29:10 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to Cartoon Story Generator</h1>
+      <p>Upload an image to generate a cartoon and a story!</p>
+    </div>
+  );
+}
